dbd15f079b6f3b681800ec450d82cc25c0659b07
vashishta <adi.vashisht@agilex.com>
2016-07-30 16:50:28 -0400

    Adding webapp functions

--- src/main/java/com/indasil/springwork/hibernate/ReservationHibernateService.java
+++ src/main/java/com/indasil/springwork/hibernate/ReservationHibernateService.java
@@ -1,5 +1,7 @@
 package com.indasil.springwork.hibernate;
 
+import javax.annotation.PostConstruct;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +11,7 @@ import com.indasil.springwork.domain.Reservation;
 
 @Service
 @Transactional
-public class ReservationHibernateService {
+public class ReservationHibernateService  {
 	
 	@Autowired
 	private SessionFactory sessionFactory;
@@ -25,6 +27,11 @@ public class ReservationHibernateService {
 	
 	public void update(Reservation r) {
 		sessionFactory.getCurrentSession().update(r);
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("Hibernate service ready!");
 	}
 
 }
